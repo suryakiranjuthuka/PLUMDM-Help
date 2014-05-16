d2b88c6ee4346e877dea3cb16f2cdbf4b5c55234
Surya Kiran Juthuka <suryakiran.kittu@gmail.com>
2014-05-16 12:06:58 -0400

    LayoutStyles

--- public/user/index.php
+++ public/user/index.php
@@ -12,35 +12,49 @@ $templates = Template::get_all_templates();
 <meta charset="utf-8">
 <meta name="google" content="notranslate">
 <meta http-equiv="Content-Language" content="en">
-<meta name="viewport" content="width=2000">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 <title>Choose a Template</title>
 <link href="../stylesheets/plum_help.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" media="only screen and (min-width: 641px) and (max-width: 1366px)" href="../stylesheets/lowres.css">
 </head>
 
 <body>
 <div id="container">
-<div id="2container">
 
-<h1 style=" font-size:16px;">Hi Surya Kiran Juthuka</h1>
+<header class="bottomShadow">
+	<div id="headerImage"><a href="www.plumdm.com"><img src="../site_images/plum_logo_name.png" height="100px"></a></div>
+</header>
+
+<section id="mainButtonsBackgroundContainer">
+    <div id="mainButtonsContainer">
+            <div class="allShadow" id="emailButton">Email</div>
+            <div class="allShadow" id="landingPageButton">Landing Pages</div>
+    </div>
+</section>
+
+<section id="allTemplatesContainer">
 
 <?php foreach($templates as $template): ?>
 		
 		<div class="full_template_container">
         
-        <div class="browser_bar"></div> 
+        <div class="browser_bar">
+        	<div class="dot1"></div>
+            <div class="dot1"></div>
+            <div class="dot1"></div>
+        </div> 
         
-        <div class="show_template"><a href="<?php echo $template->website_url; ?>"><img height="263" src="<?php echo $template->url_path; ?>"></a></div>
+        <div class="templateAllShadow show_template"><a href="<?php echo $template->website_url; ?>"><img height="263" src="<?php echo $template->url_path; ?>"></a></div>
         
-        <div class="template_website_url"><h3><?php echo $template->website_url; ?></h3></div>
+        <!--<div class="template_website_url"><h3><?php echo $template->website_url; ?></h3></div>-->
         
         </div>
         
 <?php endforeach; ?>
+</section>
+
+
 
 </div>
-
-</div>
-
-
 </body>
 </html>
